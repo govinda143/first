790ddfd488961db4876e9994d3688f7a9ef224bc
kgovinda143 <kgovindavsws@gmail.com>
2021-02-11 11:28:19 +0530

    first commit

---
+++ myscript.ps1
@@ -0,0 +1,12 @@
+for($i=1;$i -le 100;$i++)
+{
+    write out put "welcome for $i times "
+    if($i%2 -ne 0)
+    {
+        echo "$i is odd"
+    }
+    else
+    {
+      echo "$i is even"  
+    }
+}
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/ binbash
+echo"this is out off station"
